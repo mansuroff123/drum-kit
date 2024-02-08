22cd18807447b002265f353670859885487b1f67
mansuroff123 <mansuroff145@gmail.com>
2024-02-08 20:32:26 +0500

    project finish

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
 for (let i = 0; i < numberOfDrumButtons; i++) {
     
 document.querySelectorAll("button")[i].addEventListener("click", function () {
     var buttonInnerHTML = this.innerHTML;
     makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
 });
 
 }
 
 document.addEventListener("keypress", function(event) {
     makeSound(event.key);
+    buttonAnimation(event.key);
 });
 
 function makeSound(key) {
@@ -51,4 +54,13 @@ function makeSound(key) {
 
         default: console.log(buttonInnerHTML);
     }
+}
+
+function buttonAnimation(currentKey) {
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed")
+
+    setTimeout(function () {
+        activeButton.classList.remove("pressed");
+    }, 100);
 }
